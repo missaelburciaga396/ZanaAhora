4029abec255d5daf0ac977de50aceef7138c31ff
Missael Burciaga Hdz <missaelburciaga37@gmail.com>
2023-03-29 20:03:02 -0600

    arreglé el bug no se podia cambiar el idioma
    ahora puedo cambiar del español a ingles y de ingles a español

--- js/app.js
+++ js/app.js
@@ -5,10 +5,8 @@ function idioma() {
   //>console.log(check.checked);
   let id = check.checked;
   if (id == true) {
-    location.href =
-      "file:///C:/Users/Yagami/Documents/My%Web%Sites/ZanaAhora/en/index.html";
+    location.href = "/en/index.html";
   } else {
-    location.href =
-      "file:///c:/users/Yagami/Documents/My%Web%Sites/ZanaAhora/js/index.html";
+    location.href = "/js/index.html";
   }
 }
